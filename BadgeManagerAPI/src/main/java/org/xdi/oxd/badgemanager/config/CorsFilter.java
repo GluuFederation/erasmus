a2a3fc499ca080acc007d6983d8eb83a8946289f
arvindsinghtomar <tomararvindsingh09@gmail.com>
2017-04-18 19:58:58 +0530

    CORS changes

--- BadgeManagerAPI/src/main/java/org/xdi/oxd/badgemanager/config/CorsFilter.java
+++ BadgeManagerAPI/src/main/java/org/xdi/oxd/badgemanager/config/CorsFilter.java
@@ -17,7 +17,8 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         chain.doFilter(req, res);
     }
 
